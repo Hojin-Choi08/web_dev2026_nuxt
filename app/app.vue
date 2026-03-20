595c2f6d7ebd9b95e5b76d828bfac1bb0e24b6e4
Hojin Choi <merro3043@gmail.com>
2026-03-19 20:15:03 -0700

    init

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="flex flex-row justify-between gap-4 max-w-6xl mx-auto">
+    <Bucket title="title here" className="bg-green-500">
+      <strong>Lorem ipsum dolor sit amet consectetur </strong>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus autem
+      nulla atque alias eveniet necessitatibus molestias praesentium debitis
+      possimus quis. Magnam explicabo odit accusamus optio iste unde
+      reprehenderit ullam rem.
+    </Bucket>
+    <Bucket title="Another Title" className="bg-blue-600" />
+    <Bucket />
+  </section>
+</template>
